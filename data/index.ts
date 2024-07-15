d837aa1ca53dcfed53f1d8446726e210841b3615
abdallahsafa <abdallah.safa@vanderbilt.edu>
2024-07-15 10:49:01 -0400

    Style change on grid.

--- data/index.ts
+++ data/index.ts
@@ -27,11 +27,11 @@ export const gridItems = [
 
     {
         id: 3,
-        title: "",
-        description: "",
+        title: "Pride",
+        description: "my",
         className: "md:col-span-3 md:row-span-2",
         imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-        titleClassName: "justify-center md:justify-start lg:justify-center",
+        titleClassName: "mb-2",
         img: "/lions2.png",
         spareImg: "/trophy.png",
         spareImgClassName: "absolute left-0 bottom-0 w-96 h-96", // Adjust the size and position
